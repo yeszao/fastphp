fef74e6ecdde29eac9ceb65f8b77007880377a47
Gary Meng <yeszao@qq.com>
2015-09-10 18:34:41 +0800

    调整链接路径

--- application/views/item/index.php
+++ application/views/item/index.php
@@ -7,7 +7,7 @@
 <?php $number = 0?>
  
 <?php foreach ($todo as $todoitem):?>
-    <a class="big" href="../item/view/<?php echo $todoitem['Item']['id']?>/<?php echo strtolower(str_replace(" ","-",$todoitem['Item']['item_name']))?>">
+    <a class="big" href="../../item/view/<?php echo $todoitem['Item']['id']?>/<?php echo strtolower(str_replace(" ","-",$todoitem['Item']['item_name']))?>">
         <span class="item">
             <?php echo ++$number?>
             <?php echo $todoitem['Item']['item_name']?>
