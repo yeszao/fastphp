07da88a7de8ef0e256ecde7ace2a0c3716d563b2
yeszao <yeszao@qq.com>
2017-03-05 12:10:32 +0800

    update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // 数据库配置
-define('DB_NAME', 'todo');
+define('DB_NAME', 'project');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '123456');
 define('DB_HOST', 'localhost');
