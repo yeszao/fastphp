6df6858d6ced596420d23eb7500fbdb3a2e67cf8
yeszao <yeszao@qq.com>
2017-12-04 11:35:21 +0800

    用try..catch捕获PDO连接异常

--- fastphp/Db.php
+++ fastphp/Db.php
@@ -1,10 +1,11 @@
 <?php
 
 /**
- * 数据库操作类，$pdo属性为静态属性，这样在页面执行周期内，
- * 只要经过一次赋值，那么第二次再获取还是首次赋值的内容，这
- * 里就是PDO对象，这样可以确保运行期间只有一个数据库连接对
- * 像，这种是一种简单的单例模式
+ * 数据库操作类。
+ * 其$pdo属性为静态属性，所以在页面执行周期内，
+ * 只要一次赋值，以后的获取还是首次赋值的内容。
+ * 这里就是PDO对象，这样可以确保运行期间只有一个
+ * 数据库连接对象，这是一种简单的单例模式
  * Class Db
  */
 class Db
@@ -17,9 +18,13 @@ class Db
             return self::$pdo;
         }
 
-        $dsn = sprintf('mysql:host=%s;dbname=%s', DB_HOST, DB_NAME);
-        $option = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+        try {
+            $dsn    = sprintf('mysql:host=%s;dbname=%s;charset=utf8', DB_HOST, DB_NAME);
+            $option = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
 
-        return self::$pdo = new PDO($dsn, DB_USER, DB_PASS, $option);
+            return self::$pdo = new PDO($dsn, DB_USER, DB_PASS, $option);
+        } catch (PDOException $e) {
+            exit($e->getMessage());
+        }
     }
 }
