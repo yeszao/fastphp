99886508430c94a81bd718f95e8d55a74ed5d6a0
Gary Meng <yeszao@qq.com>
2018-08-14 10:46:20 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 // 数据库配置
 $config['db']['host'] = 'localhost';
 $config['db']['username'] = 'root';
-$config['db']['password'] = 'root';
-$config['db']['dbname'] = 'demo';
+$config['db']['password'] = '123456';
+$config['db']['dbname'] = 'project';
 
 // 默认控制器和操作名
 $config['defaultController'] = 'Item';
