a0660c3b237e718ed956c9cc9eeab2ea26627151
Guozao Meng <3072379+yeszao@users.noreply.github.com>
2024-05-28 21:51:37 +0400

    Create [config_copy/config.php]

---
+++ config_copy/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// 数据库配置
+$config['db']['host'] = 'mysql';
+$config['db']['username'] = 'root';
+$config['db']['password'] = '123456';
+$config['db']['dbname'] = 'project';
+
+// 默认控制器和操作名
+$config['defaultController'] = 'Item';
+$config['defaultAction'] = 'index';
+
+return $config;
